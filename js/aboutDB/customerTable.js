cdfe890ecf52e146435d336a3df65d63fcc218c1
kijk6010 <kijk6010@naver.com>
2020-07-10 18:06:04 +0900

    CustomerPage 테이블 기본 완성

--- js/aboutDB/customerTable.js
+++ js/aboutDB/customerTable.js
@@ -32,15 +32,49 @@ window.onload = function() {
 
         if(AppWeb!=undefined){
             customerData.innerHTML +='<tr>\n' +
-                '                                    <td>'+AppWeb+'</td>\n' +
-                '                                   <td>'+clientName+'</td>\n' +
-                '                                  <td>'+device+'</td>\n' +
-                '                                   <td>'+age+'</td>\n' +
-                '                                   <td>'+testDetails+'</td>\n' +
-                '                                   <td>'+clientEmail+'</td>\n' +
-                '                                   <td>'+clientTel+'</td>\n' +
+                '                                    <td style="width:11%;">'+AppWeb+'</td>\n' +
+                '                                   <td style="width:12%;">'+clientName+'</td>\n' +
+                '                                  <td style="width:12%;">'+device+'</td>\n' +
+                '                                   <td style="width:14%;">'+age+'</td>\n' +
+                '                                   <td style="width:24%;">'+testDetails+'</td>\n' +
+                '                                   <td style="width:14%;">'+clientEmail+'</td>\n' +
+                '                                   <td style="width:14%;">'+clientTel+'</td>\n' +
                 '                                </tr>' ;
         }
     });
 };
 
+function myFunction() {
+    var radiogaga = document.getElementsByName("search1");
+
+    // Declare variables
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("customerData");
+    tr = table.getElementsByTagName("tr");
+
+    // Loop through all table rows, and hide those who don't match the search query
+    for (i = 0; i < tr.length; i++) {
+        //[]인덱스 안에 번호에 따라서 name의 검색할지 Countrty에 검색할지 결정
+        //td = tr[i].getElementsByTagName("td")[1];
+        if(radiogaga[0].selected){
+            td=tr[i];
+        }
+        else {
+            for (var j = 1; j < radiogaga.length; j++) {
+                if (radiogaga[j].selected) {
+                    td = tr[i].getElementsByTagName("td")[j-1];
+                }
+            }
+        }
+        if (td) {
+            txtValue = td.textContent || td.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+        }
+    }
+}
