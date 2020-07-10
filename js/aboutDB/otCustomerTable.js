f01bdea8f69f5216896f22b67abd02151ac0032d
kijk6010 <kijk6010@naver.com>
2020-07-10 18:57:41 +0900

    OT_Customer 페이지 틀

--- js/aboutDB/otCustomerTable.js
+++ js/aboutDB/otCustomerTable.js
@@ -16,6 +16,7 @@ window.onload = function() {
 
 
     var customerData = document.getElementById("customerData");
+    var customerData2 = document.getElementById("customerData2");
 
     var database = firebase.database();
 
@@ -32,15 +33,95 @@ window.onload = function() {
         var testCheck = data.L_onCheck;
         if(AppWeb!=undefined&&testCheck==1){
             customerData.innerHTML +='<tr>\n' +
-                '                                    <td>'+AppWeb+'</td>\n' +
-                '                                   <td>'+clientName+'</td>\n' +
-                '                                   <td>'+device+'</td>\n' +
-                '                                   <td>'+age+'</td>\n' +
-                '                                   <td>'+testDetails+'</td>\n' +
-                '                                   <td>'+clientEmail+'</td>\n' +
-                '                                   <td>'+clientTel+'</td>\n' +
+                '                                    <td style="width:7%;">'+AppWeb+'</td>\n' +
+                '                                   <td style="width:8%;">'+clientName+'</td>\n' +
+                '                                  <td style="width:8%;">'+device+'</td>\n' +
+                '                                   <td style="width:9%;">'+age+'</td>\n' +
+                '                                   <td style="width:16%;">'+testDetails+'</td>\n' +
+                '                                   <td style="width:6%;">'+clientEmail+'</td>\n' +
+                '                                   <td style="width:6%;">'+clientTel+'</td>\n' +
+                '                                </tr>' ;
+        }
+        if(AppWeb!=undefined&&testCheck==0){
+            customerData2.innerHTML +='<tr>\n' +
+                '                                    <td style="width:7%;">'+AppWeb+'</td>\n' +
+                '                                   <td style="width:8%;">'+clientName+'</td>\n' +
+                '                                  <td style="width:8%;">'+device+'</td>\n' +
+                '                                   <td style="width:9%;">'+age+'</td>\n' +
+                '                                   <td style="width:16%;">'+testDetails+'</td>\n' +
+                '                                   <td style="width:6%;">'+clientEmail+'</td>\n' +
+                '                                   <td style="width:6%;">'+clientTel+'</td>\n' +
                 '                                </tr>' ;
         }
     });
 };
 
+//검색 부분
+function myFunction() {
+    var radiogaga = document.getElementsByName("search1");
+
+    // Declare variables
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("customerData");
+    tr = table.getElementsByTagName("tr");
+
+    // Loop through all table rows, and hide those who don't match the search query
+    for (i = 0; i < tr.length; i++) {
+        //[]인덱스 안에 번호에 따라서 name의 검색할지 Countrty에 검색할지 결정
+        //td = tr[i].getElementsByTagName("td")[1];
+        if(radiogaga[0].selected){
+            td=tr[i];
+        }
+        else {
+            for (var j = 1; j < radiogaga.length; j++) {
+                if (radiogaga[j].selected) {
+                    td = tr[i].getElementsByTagName("td")[j-1];
+                }
+            }
+        }
+        if (td) {
+            txtValue = td.textContent || td.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+        }
+    }
+}
+function myFunction2() {
+    var radiogaga2 = document.getElementsByName("search2");
+
+    // Declare variables
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("myInput2");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("customerData2");
+    tr = table.getElementsByTagName("tr");
+
+    // Loop through all table rows, and hide those who don't match the search query
+    for (i = 0; i < tr.length; i++) {
+        //[]인덱스 안에 번호에 따라서 name의 검색할지 Countrty에 검색할지 결정
+        //td = tr[i].getElementsByTagName("td")[1];
+        if(radiogaga2[0].selected){
+            td=tr[i];
+        }
+        else {
+            for (var j = 1; j < radiogaga2.length; j++) {
+                if (radiogaga2[j].selected) {
+                    td = tr[i].getElementsByTagName("td")[j-1];
+                }
+            }
+        }
+        if (td) {
+            txtValue = td.textContent || td.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+        }
+    }
+}
